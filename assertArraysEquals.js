3bf2525d5091385afda4a5f805e20aee3c394e81
Kendall Rowe <kendall.rowe312@gmail.com>
2019-06-25 19:21:12 +0000

    Linted

--- assertArraysEquals.js
+++ assertArraysEquals.js
@@ -23,8 +23,8 @@ const eqArrays = function(arr1, arr2) {
 };
 
 // TEST CODE
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3])) // => true
-console.log(assertArraysEqual([1, 2, 3], [3, 2, 1])) // => false
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3])); // => true
+console.log(assertArraysEqual([1, 2, 3], [3, 2, 1])); // => false
 
-console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"])) // => true
-console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3])) // => false
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3])); // => false
