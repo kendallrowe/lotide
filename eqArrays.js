7e158e8b22ceae1f654407c0f08ce5929a4f47cf
Kendall Rowe <kendall.rowe312gmail.com>
2019-06-25 19:21:12 +0000

    Linted

--- eqArrays.js
+++ eqArrays.js
@@ -23,8 +23,8 @@ const eqArrays = function(arr1, arr2) {
 };
 
 // TEST CODE
-console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true)) // => true
-console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false)) // => false
+console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true)); // => true
+console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false)); // => false
 
-console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true)) // => true
-console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]),false)) // => false
+console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]),true)); // => true
+console.log(assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]),false)); // => false
