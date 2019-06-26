1b9c8252366b046693f920a8a606262ac5f26c08
Kendall Rowe <kendall.rowe312gmail.com>
2019-06-26 16:31:17 +0000

    Created file from lesson

---
+++ countOnly.js
@@ -0,0 +1,46 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  const match = actual === expected;
+  if (match === true) {
+    return `😻😻😻 Assertion Passed: ${actual} === ` + expected;
+  } else {
+    return `🙈🙈🙈 Assertion Failed: ${actual} !== ` + expected;
+  }
+};
+
+const countOnly = function(allItems, itemsToCount){
+  const results = {
+    item: 0
+  };
+
+  for (const item of allItems) {
+    if (itemsToCount[item]) { 
+      if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
+      }
+    }
+  }
+
+  return results;
+}
+
+// TEST CODE
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true });
+console.log(result1);
+console.log(assertEqual(result1["Jason"], undefined));
+console.log(assertEqual(result1["Karima"], undefined));
+console.log(assertEqual(result1["Fang"], 2));
