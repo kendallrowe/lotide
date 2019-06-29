c4eef61f35191626bc159bdb587c43590dce7bb7
Kendall Rowe <kendall.rowe312@gmail.com>
2019-06-29 00:05:12 +0000

    Added recursion to eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -14,7 +14,11 @@ const eqArrays = function(arr1, arr2) {
   }
 
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      if (!eqArrays(arr1[i], arr2[i])) {
+        return false;
+      }
+    } else if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
@@ -23,6 +27,11 @@ const eqArrays = function(arr1, arr2) {
 };
 
 // TEST CODE
+console.log(assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4]]), true)) // => true
+
+console.log(assertEqual(eqArrays([[2, 3], [4]], [[2, 3], [4, 5]]), false)) // => false
+console.log(assertEqual(eqArrays([[2, 3], [4]], [[2, 3], 4]), false)) // => false
+
 console.log(assertEqual(eqArrays([1, 2, 3], [1, 2, 3]),true)); // => true
 console.log(assertEqual(eqArrays([1, 2, 3], [3, 2, 1]),false)); // => false
 
