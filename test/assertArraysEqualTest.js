046127fec3cc78c809fad5a9ae3203c5d5029c8c
Kendall Rowe <kendall.rowe312@gmail.com>
2019-07-01 13:05:26 +0000

    Modularized and created test file for assertArraysEquals

---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,8 @@
+const assertArraysEqual = require("../assertArraysEquals");
+
+// TEST CODE
+console.log(assertArraysEqual([1, 2, 3], [1, 2, 3])); // => true
+console.log(assertArraysEqual([1, 2, 3], [3, 2, 1])); // => false
+
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"])); // => true
+console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3])); // => false
